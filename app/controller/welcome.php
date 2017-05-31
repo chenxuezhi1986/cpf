3e508ff7cc0cf4557b3c1f6e7621c1fe0fca1872
chenxuezhi1986 <chenxuezhi1986@126.com>
2017-05-31 13:54:36 +0800

    测试

--- app/controller/welcome.php
+++ app/controller/welcome.php
@@ -10,8 +10,8 @@ class Welcome_Controller extends Controller_Base{
     public function index()
     {
         load::model('welcome_model');
-        $data['title'] = '欢迎使用CPF框架';
+        $data['title'] = 'ooo';
         $data['content'] = $this->welcome_model->hi();
-        tpl::init()->display('welcome', $data);
+        tpl::display('welcome', $data);
     }
 }
